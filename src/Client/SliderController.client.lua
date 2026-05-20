2b1b456c25fafac84c35f647c028c216f34236ce
Peter <peterrafat25@gmail.com>
2026-05-20 14:33:47 +0300

    UI Polishing

--- src/Client/SliderController.client.lua
+++ src/Client/SliderController.client.lua
@@ -6,7 +6,7 @@ local player = Players.LocalPlayer
 local changeIntervalRemote = ReplicatedStorage:WaitForChild("Shared"):WaitForChild("Remotes"):WaitForChild("ChangeInterval")
 
 local playerGui = player:WaitForChild("PlayerGui")
-local controlPanel = playerGui:WaitForChild("ControlPanel", 99999) 
+local controlPanel = playerGui:WaitForChild("ControlPanel", 99999)
 
 if controlPanel then
 	local frame = controlPanel:WaitForChild("Frame")
@@ -16,6 +16,22 @@ if controlPanel then
 	local dragging = false
 	local minInterval = 0.1
 	local maxInterval = 5.0
+	local defaultInterval = 1.0 
+
+	task.wait()
+
+	local function updateSliderVisuals(percentage)
+		local frameSize = frame.AbsoluteSize.X
+		local btnSize = sliderBtn.AbsoluteSize.X
+		local maxOffset = frameSize - btnSize
+		
+		local targetOffset = percentage * maxOffset
+		sliderBtn.Position = UDim2.new(0, targetOffset, sliderBtn.Position.Y.Scale, sliderBtn.Position.Y.Offset)
+	end
+
+	local initialPercentage = (defaultInterval - minInterval) / (maxInterval - minInterval)
+	updateSliderVisuals(initialPercentage)
+	textLabel.Text = "Interval: " .. tostring(defaultInterval) .. "s"
 	
 	sliderBtn.MouseButton1Down:Connect(function()
 		dragging = true
@@ -32,19 +48,25 @@ if controlPanel then
 			local mousePos = UserInputService:GetMouseLocation().X
 			local framePos = frame.AbsolutePosition.X
 			local frameSize = frame.AbsoluteSize.X
+			local btnSize = sliderBtn.AbsoluteSize.X
 			
-			local relativePos = math.clamp(mousePos - framePos, 0, frameSize)
-			local percentage = relativePos / frameSize
+			local maxMovement = frameSize - btnSize
+			local relativePos = math.clamp(mousePos - framePos, 0, maxMovement)
 			
-			sliderBtn.Position = UDim2.new(percentage, 0, sliderBtn.Position.Y.Scale, sliderBtn.Position.Y.Offset)
+			local percentage = relativePos / maxMovement
+			updateSliderVisuals(percentage)
 			
 			local currentInterval = minInterval + ((maxInterval - minInterval) * percentage)
-			
 			currentInterval = math.floor(currentInterval * 10) / 10
 			
 			textLabel.Text = "Interval: " .. tostring(currentInterval) .. "s"
-			
 			changeIntervalRemote:FireServer(currentInterval)
 		end
+	end)
+	
+	frame:GetPropertyChangedSignal("AbsoluteSize"):Connect(function()
+		local currentIntervalVal = tonumber(string.match(textLabel.Text, "%d+%.%d")) or defaultInterval
+		local currentPerc = (currentIntervalVal - minInterval) / (maxInterval - minInterval)
+		updateSliderVisuals(currentPerc)
 	end)
 end
